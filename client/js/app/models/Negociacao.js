225b13fbfd762b339ffd1a2a9a87ec3630bb539f
fabriciamartins <fabricia.silva@linksolutions.com.br>
2019-01-21 23:49:27 -0200

    first commit

---
+++ client/js/app/models/Negociacao.js
@@ -0,0 +1,15 @@
+class Negociacao {
+
+    constructor(data, quantidade, valor){
+
+        this.data = data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+
+    }
+
+    getVolume(){
+        return this.quantidade * this.valor;
+    }
+
+}
